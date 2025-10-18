e2722497128f41d942c969b12696a6361602193f
gadingsuksess <gadingsuksess1@gmail.com>
2025-10-18 16:26:56 +0700

    Dasar C#

---
+++ Pertemuan 30-9-2025 C#/Latihan1_Gading Azriel P._XPPLG1/Latihan1_Gading Azriel P._XPPLG1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan1_Gading_Azriel_P._XPPLG1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("=== Mengubah suhu celcius ke fahrenheit dan reamur ===");
+            Console.Write("Masukkan suhu celcius : ");
+            double c = Convert.ToDouble(Console.ReadLine());
+            string f = "°F";
+            string re = "°Re";
+
+            //Eksekusi
+            //Fahrenheit
+            double fx = (c * 9 / 5) + 32;
+
+            //Reamur
+            double fy = (c * 4 / 5);
+
+            //Hasil
+            Console.WriteLine("=== Hasil Konversi ===");
+            Console.WriteLine($"Celcius : " + (fx) + (f));
+            Console.WriteLine($"Reamur  : " + (fy) + (re));
+
+        }
+    }
+}
---
+++ Pertemuan 30-9-2025 C#/Latihan2_Gading Azriel P._XPPLG1/Latihan2_Gading Azriel P._XPPLG1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan2_Gading_Azriel_P._XPPLG1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Mengkonversi nilai tukar uang ===");
+            string x = "Rp";
+            Console.Write("Masukkan jumlah uang dalam bentuk Rupiah (IDR) : " + x);
+            double rp = (Convert.ToDouble(Console.ReadLine()));
+
+            //Eksekusi Sesuai nilai kurs sekarang
+            double usd = rp / 16669;
+            double gbp = rp / 22397;
+            double jpy = rp / 112;
+            double sar = rp / 4444;
+
+            //Hasil
+            Console.WriteLine("=== Hasil ===");
+            Console.WriteLine("Jumlah Rupiah            : Rp " + rp.ToString("N0"));
+            Console.WriteLine("Ke Dolar AS              : $ " + usd.ToString("N2"));
+            Console.WriteLine("Ke Poundsterling Inggris : £ " + gbp.ToString("N2"));
+            Console.WriteLine("Ke Yen Jepang            : ¥ " + jpy.ToString("N2"));
+            Console.WriteLine("Ke Riyal Saudi           : ﷼ " + sar.ToString("N2"));
+
+        }
+    }
+}
